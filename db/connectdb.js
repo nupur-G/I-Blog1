83584a51ef3d9c71aa5477616476a9ec59e4d6be
nupur-G <guptanupur867@gmail.com>
2024-05-28 13:26:12 +0530

    fhhjfgh

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,10 +1,11 @@
 // Database creation require and call this function in app.js
 const mongoose = require('mongoose')
 localurl = "mongodb://127.0.0.1:27017/Blog_1";
+liveUrl ="mongodb+srv://guptanupur867:gupta123@blog-app.wjjq8yj.mongodb.net/blog?retryWrites=true&w=majority&appName=blog-app"
 
 
 const connectDB = () => {
-    return mongoose.connect(localurl) // mongoose.connect hime return mai promise deta hai
+    return mongoose.connect(liveUrl) // mongoose.connect hime return mai promise deta hai
     .then(() => {
         console.log('connected successfully')
     }).catch((error)=>{
